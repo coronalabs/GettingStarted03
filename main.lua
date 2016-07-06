2482727dbb63232b83eb3996150ecff0c57051ba
Rob Miracle <rob@coronalabs.com>
2016-07-06 15:36:32 -0400

    making code match the text
    Added randomized to main.lua

--- main.lua
+++ main.lua
@@ -3,6 +3,9 @@ local physics = require( "physics" )
 physics.start()
 physics.setGravity( 0, 0 )
 
+-- Seed the random number generator
+math.randomseed( os.time() )
+
 -- Configure image sheet
 local options =
 {
