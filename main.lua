476ecb0050e14cf837d1337ae96de2e9b7edc022
ignisdesign <bjsorrentino@hotmail.com>
2017-02-08 14:23:20 -1000

    Update main.lua

--- main.lua
+++ main.lua
@@ -46,7 +46,7 @@ local sheetOptions =
             width = 14,
             height = 40
         },
-    }
+    },
 }
 local objectSheet = graphics.newImageSheet( "gameObjects.png", sheetOptions )
 
